cb2d505da35c80234d1bdc04647b55b64374e1cf
Ankityadav98 <2016034@iiitdmj.ac.in>
2024-11-15 12:38:57 +0530

    Tic tac toe final files

---
+++ README.md
@@ -0,0 +1,2 @@
+# This is a website made using Javascript, html5 and CSS3 for playing the tic tac toe game.
+## Javascript Concepts used are DOMs, querySelectors, eventListener etc.
---
+++ app.js
@@ -0,0 +1,80 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#resetBtn");
+let newgameBtn = document.querySelector("#newgame-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+console.dir(boxes);
+
+let turnO = true;
+const win = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+newgameBtn.addEventListener("click", () => {
+  enableBoxes();
+  msgContainer.classList.add("hide");
+});
+
+resetBtn.addEventListener("click", () => {
+  enableBoxes();
+  if (!msgContainer.classList.contains("hide")) {
+    msgContainer.classList.add("hide");
+  }
+});
+
+const enableBoxes = () => {
+  turnO = true;
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const checkWinner = () => {
+  for (let pattern of win) {
+    const pos1val = boxes[pattern[0]].innerText;
+    const pos2val = boxes[pattern[1]].innerText;
+    const pos3val = boxes[pattern[2]].innerText;
+
+    if (pos1val != "" && pos2val != "" && pos3val != "") {
+      if (pos1val === pos2val && pos2val === pos3val) {
+        showWinner(pos1val);
+        disableBoxes();
+      }
+    }
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+};
